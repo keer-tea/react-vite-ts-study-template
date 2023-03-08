c617569f2dc3c96a7584ae867dc0f9629aec0713
lukecheng <lukecheng233@163.com>
2023-03-08 10:39:05 +0800

    feat: 修改环境判断

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-if (import.meta.env.MODE === 'test') {
+if (import.meta.env.PROD) {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   import('https://cdn.staticfile.org/vConsole/3.3.4/vconsole.min.js').then(() => {
