cfc5f5cc1565b94d43bd573aa9b432617ec50f79
lukecheng <lukecheng233@163.com>
2023-05-22 18:13:23 +0800

    prod init

--- src/pages/loading/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './index.scss'
-
-const Index =  () => {
-  return <div>loading 中。。。。。</div>
-}
-
-export default Index
--- src/store/index.ts
+++
@@ -1,6 +0,0 @@
-import { useContext, createContext } from 'react'
-import homeStore from './module/homeStore'
-
-export const allStore = {
-  homeStore
-}
