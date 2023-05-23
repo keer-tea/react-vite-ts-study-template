604267a04c79ceaa15f7427e2408903f3faced91
lukecheng <lukecheng233@163.com>
2023-05-24 01:33:18 +0800

    test

--- utilities/union/pre/assets/index-legacy-2f9a2736.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-1e973095.js"],(function(e,n){"use strict";var t,r,c,i;return{setters:[function(e){t=e.u,r=e.j,c=e.F,i=e.a}],execute:function(){e("default",(function(){var e=t();return r(c,{children:[i("div",{children:"record 页"}),i("button",{onClick:function(){e(-1)},children:"返回"})]})}))}}}));
--- utilities/union/prod/assets/index-43f4b7ef.js
+++
@@ -1,1 +0,0 @@
-import{u as n,j as t,F as i,a}from"./index-d63a9cae.js";const e=()=>{const e=n();return t(i,{children:[a("div",{children:"detail 页"}),a("button",{onClick:function(){e(-1)},children:"返回"})]})};export{e as default};
