3f9ed29e935205786f11f2fadeeb33e1ce699ab4
lukecheng <lukecheng233@163.com>
2023-05-23 09:48:05 +0800

    del files

--- src/hooks/usePow.ts
+++
@@ -1,9 +0,0 @@
-import { useMemo } from 'react'
-
-export default (list: number[]) => {
-  return useMemo(() => list.map(num => {
-    console.log('hook 执行了')
-    return Math.pow(num, 2)
-  }), [])
-}
-console.log(123)
